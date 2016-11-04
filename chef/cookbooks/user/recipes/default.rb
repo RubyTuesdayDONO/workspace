913178d7c6978a991dfdab8c4505837d7d5294ab
Reuben Garrett <ruby@rubydono.net>
2016-11-04 02:29:05 -0500

    Initialize Chef repo
    Initial commit of Chef repo, with trivially-generated
    cookbook that installs a few packages and directories.
    Intented for testing chef-zero in Crouton environment.
    
    TODO: - break-out system resources into separate cookbook
            so user cookbook can converge without privilege.
          - implement secrets cookbook using (e.g.) Hashicorp
            Vault to enable external storage of private data.
          - setup Test Kitchen and other unit testing / linting.

---
+++ chef/cookbooks/user/recipes/default.rb
@@ -0,0 +1,19 @@
+#
+# Cookbook Name:: user
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+package 'ruby'
+package 'git'
+package 'rubygems'
+
+gem 'vagrant'
+gem 'packer'
+gem 'test-kitchen'
+
+user ='reuben'
+
+['~/src/com'].each do |dir|
+  directory dir.gsub(/^~/, "/home/#{user}")
+end
